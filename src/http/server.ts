e0744d18eb013a0527f2f0883d1427d80c86fe14
LucasMeloSena <lucasmelosena@gmail.com>
2024-02-08 21:08:11 -0300

    developed all system

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,10 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
+import websocket from '@fastify/websocket'
 import { createPoll } from 'src/routes/create-poll'
 import { getPoll } from 'src/routes/get-poll'
 import { voteOnPoll } from 'src/routes/vote-on-poll'
+import { pollResults } from './websocket/poll-results'
 
 const app = fastify()
 
@@ -11,9 +13,11 @@ app.register(cookie, {
     hook: "onRequest",
 })
 
+app.register(websocket)
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+app.register(pollResults)
 
 app.listen({ port: 3333 }).then(() => {
     console.log("HTTP server running")
--- src/routes/vote-on-poll.ts
+++ src/routes/vote-on-poll.ts
@@ -2,6 +2,8 @@ import { z } from 'zod'
 import { prisma } from "src/lib/prisma"
 import { FastifyInstance } from 'fastify'
 import { randomUUID } from 'crypto'
+import { redis } from 'src/lib/redis'
+import { voting } from 'src/utils/voting-pub-sub'
 
 export async function voteOnPoll(app: FastifyInstance) {
     app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -31,6 +33,11 @@ export async function voteOnPoll(app: FastifyInstance) {
                         id: userPreviousVoteOnPoll.id
                     }
                 })
+                const lastVotes = await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+                voting.publish(pollId, {
+                    pollOptionId: userPreviousVoteOnPoll.pollOptionId,
+                    votes: Number(lastVotes)
+                })
             }
             else if (userPreviousVoteOnPoll) {
                 reply.status(400).send({ message: "You already voted in this poll!" })
@@ -53,6 +60,13 @@ export async function voteOnPoll(app: FastifyInstance) {
                 pollId,
                 pollOptionId
             }
+        })
+
+        const votes = await redis.zincrby(pollId, 1, pollOptionId)
+
+        voting.publish(pollId, {
+            pollOptionId,
+            votes: Number(votes)
         })
 
         return reply.status(201).send({ sessionId })
